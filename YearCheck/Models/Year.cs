50e5b338329215b5acf97ca8df714856d6508e73
Mariia <mariiapopovych@gmail.com>
2019-04-25 07:52:10 -0700

    first commit

---
+++ YearCheck/Models/Year.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+
+namespace Year.Models
+{
+ public class Date // class
+ {
+   private int _year; // properties
+   private int _month;
+   private int _day;
+
+   public Date (int year, int month, int day)
+   {
+     _year = year;
+     _month = month;
+   }
+
+ }
+}
